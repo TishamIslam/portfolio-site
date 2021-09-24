ba27ea74f1e5fa0c53132a48ea888d64270a3935
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-09-24 14:56:25 -0400

    add TheHeader comp + navigation + del default index,js

--- components/atoms/singletons/TheHeader.js
+++ components/atoms/singletons/TheHeader.js
@@ -1,10 +1,21 @@
+import Link from 'next/link'
+
 export default function TheHeader() {
   return (
-    <div className="container mx-auto">
-      <h1 className="ml-6 text-6xl ">This is the title</h1>
+    <div className="w-3/4 absolute top-4">
+      <h1 className="text-6xl ">This is the title</h1>
       <nav>
-        <ul>
-          <li></li>
+        <ul className="list-none my-2 p-2 space-x-2 bg-purple-900 rounded text-white">
+          <li className="list-menu-item rounded-l-md">
+            <Link href="/">
+              <a className="dark-bg">Home</a>
+            </Link>
+          </li>
+          <li className="list-menu-item">
+            <Link href="/tic-tac-toe">
+              <a className="dark-bg">Tic-Tac-Toe</a>
+            </Link>
+          </li>
         </ul>
       </nav>
     </div>
