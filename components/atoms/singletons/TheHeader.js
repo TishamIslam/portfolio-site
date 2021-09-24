8bcba4d559243527a5780dde9b5bba1084b6fdc8
Tisham Islam <45071113+TishamIslam@users.noreply.github.com>
2021-09-24 13:58:06 -0400

    npm i ran, settings file for recommended vscode settings added

---
+++ components/atoms/singletons/TheHeader.js
@@ -0,0 +1,12 @@
+export default function TheHeader() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="ml-6 text-6xl ">This is the title</h1>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </div>
+  )
+}
